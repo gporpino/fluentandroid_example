e796fde8b97190e8fc21b612ea7ac264e8804751
Gustavo Porpino <gpc@cesar.org.br>
2014-02-25 17:47:32 -0300

    Implementing some constraints

--- src/com/example/fluentandroidexample/MainActivity.java
+++ src/com/example/fluentandroidexample/MainActivity.java
@@ -4,10 +4,10 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.widget.TextView;
-import br.org.cesar.gporpino.fluentandroid.database.CursorLine;
+import br.org.cesar.gporpino.fluentandroid.database.CursorEntry;
 import br.org.cesar.gporpino.fluentandroid.database.FluentCursor;
-import br.org.cesar.gporpino.fluentandroid.direct.Insert;
-import br.org.cesar.gporpino.fluentandroid.direct.Select;
+import br.org.cesar.gporpino.fluentandroid.database.direct.Insert;
+import br.org.cesar.gporpino.fluentandroid.database.direct.Select;
 
 import com.example.fluentandroidexample.FeedReaderContract.FeedEntry;
 
@@ -18,7 +18,7 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		int count = new Select("COUNT(*)").from(FeedEntry.TABLE_NAME).execute().getCursor().getInt(0);
+		int count = new br.org.cesar.gporpino.fluentandroid.database.direct.Select("COUNT(*)").from(FeedEntry.TABLE_NAME).execute().getCursor().getInt(0);
 		
 		new Insert().into(FeedEntry.TABLE_NAME)
 			.value(FeedEntry.COLUMN_NAME_ENTRY_ID, count)
@@ -32,7 +32,7 @@ public class MainActivity extends Activity {
 		
 		if (cursor != null){
 			StringBuilder sb = new StringBuilder();
-			for (CursorLine line : cursor) {
+			for (CursorEntry line : cursor) {
 				sb.append(line.getString(FeedEntry.COLUMN_NAME_TITLE));
 			}
 			TextView text = (TextView)findViewById(R.id.text);
